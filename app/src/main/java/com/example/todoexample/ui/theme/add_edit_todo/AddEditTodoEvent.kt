01efb3feb017f02b3a4181b9ae1bd86e32b0479b
Yomal <freezyomal2@gmail.com>
2022-12-13 00:02:13 +0530

    Ui creation

---
+++ app/src/main/java/com/example/todoexample/ui/theme/add_edit_todo/AddEditTodoEvent.kt
@@ -0,0 +1,3 @@
+package com.example.todoexample.ui.theme.add_edit_todo
+
+sealed class AddEditTodoEvent
--- app/src/main/java/com/example/todoexample/ui/theme/add_edit_todo/AddEditTodoViewModel.kt
+++ app/src/main/java/com/example/todoexample/ui/theme/add_edit_todo/AddEditTodoViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.todoexample.ui.theme.add_edit_todo
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoexample.data.Todo
 import com.example.todoexample.data.TodoRepository
+import com.example.todoexample.util.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +20,33 @@ class AddEditTodoViewModel @Inject constructor(
     private val repository: TodoRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+
+    var todo by mutableStateOf<Todo?>(null)
+    private set
+
+    var title by mutableStateOf("")
+    private set
+
+    var description by mutableStateOf("")
+    private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    init {
+        val todoId = savedStateHandle.get<Int>("todoId")!!
+        if (todoId != -1){
+            viewModelScope.launch {
+                repository.getTodoById(todoId)?.let { todo ->
+                    title = todo.title
+                    description = todo.description ?: ""
+                    this@AddEditTodoViewModel.todo = todo
+
+                }
+
+            }
+        }
+    }
+
+    //fun onEvent(event: )
 }
